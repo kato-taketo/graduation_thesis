c4dc9c36b9fa878b3f6f3e5640cabafacb27627e
tkato10315 <take10tokyohkit@gmail.com>
2022-12-05 14:33:14 +0900

    add loc catch_copy

--- add_catch_copy.py
+++ add_catch_copy.py
@@ -1,18 +1,31 @@
 from PIL import Image,ImageFont,ImageDraw
 import os
 
-def add_catch_copy(image_path, catch_copy):
+def add_catch_copy(image_path, catch_copy, loc_catch_copy):
     '''
     Use the image path and catchphrase string as input to draw catchphrase on the image
     '''
     font_path = '/usr/share/fonts/truetype/Gargi/Gargi.ttf'
     font_size = 30 #文字の大きさ
     color = (255,255,255)#文字の色
-
+    
+    loc = find_loc_catch_copy(loc_catch_copy)
     font = ImageFont.truetype(font_path,font_size)#フォントの指定
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
-    draw.text((50,100),catch_copy,font=font,fill=color)
+    draw.text(loc,catch_copy,font=font,fill=color)
     image.save(image_path)
+
+def find_loc_catch_copy(loc_catch_copy):
+    locs_str = ["Top_Left", "Top", "Top_Right", "Center_Left", "Center", "Center_Right", "Bottom_Left", "Bottom", "Bottom_Right"]
+    locs = [(10,10),(95,10),(180,10),
+            (10,95),(95,95),(180,95),
+            (10,180),(95,180),(180,180) ]
+    for i in range(len(locs_str)):
+        if loc_catch_copy == locs_str[i]:
+            loc = locs[i]
+
+    return loc
+
 
  
